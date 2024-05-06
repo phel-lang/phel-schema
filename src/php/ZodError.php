88fcbe137e28206b4b7124bbeba92cd56ac27a48
smeghead <smeghead7@gmail.com>
2024-05-06 22:41:34 +0900

    feat: add ZodError class.

---
+++ src/php/ZodError.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Smeghead\PhelSchema;
+
+final class ZodError extends \RuntimeException {
+  private array $issues = [];
+
+  public static function create(string $message, array $issues): self
+  {
+    $self = new self($message);
+    $self->issues = $issues;
+    return $self;
+  }
+
+  public function getIssues(): array
+  {
+    return $this->issues;
+  }
+}
